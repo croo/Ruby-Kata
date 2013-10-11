38a9fb3bccfb25180db73ae2d1eac64e10d7dc0c
Croo <croohmanoor@gmail.com>
2013-10-11 23:34:27 +0200

    Linked the files together.

--- FizzBuzz/FizzBuzzTest.rb
+++ FizzBuzz/FizzBuzzTest.rb
@@ -1,11 +1,12 @@
 require 'test/unit'
+require_relative 'FizzBuzz'
 
 
 class FizzBuzzTest < Test::Unit::TestCase
 
     def test_classExists
-        s = "Hello, World!"
-        assert_equal(13, s.length)
+        fizzBuzz = FizzBuzz.new
+        assert_equal("hello", fizzBuzz.hello)
     end
 end
 
