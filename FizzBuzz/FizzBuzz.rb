d8cb1d0f1cafa8d83ade740fb88b85b723d22cdd
Croo <croohmanoor@gmail.com>
2013-10-12 01:04:55 +0200

    Fizzbuzz kata done.

--- FizzBuzz/FizzBuzz.rb
+++ FizzBuzz/FizzBuzz.rb
@@ -1,7 +1,15 @@
 
 class FizzBuzz
 
-    def hello
-        return "hello"
+    def get(number)
+        if(number % 3 == 0 && number % 5 == 0)
+            return "fizzbuzz"
+        elsif(number % 3 == 0)
+            return "fizz"
+        elsif( number % 5 == 0)
+            return "buzz"
+        else
+            return number
+        end
     end
 end
--- FizzBuzz/FizzBuzzTest.rb
+++ FizzBuzz/FizzBuzzTest.rb
@@ -4,9 +4,32 @@ require_relative 'FizzBuzz'
 
 class FizzBuzzTest < Test::Unit::TestCase
 
-    def test_classExists
+    def test_class_and_function_exists
         fizzBuzz = FizzBuzz.new
-        assert_equal("hello", fizzBuzz.hello)
+        assert_equal(1, fizzBuzz.get(1))
+    end
+
+    def test_should_return_number
+        fizzBuzz = FizzBuzz.new
+        assert_equal(2, fizzBuzz.get(2))
+    end
+
+    def test_dividable_three_should_return_fizz
+        fizzBuzz = FizzBuzz.new
+        assert_equal("fizz", fizzBuzz.get(3))
+        assert_equal("fizz", fizzBuzz.get(9))
+    end
+
+    def test_dividable_five_should_return_buzz
+        fizzBuzz = FizzBuzz.new
+        assert_equal("buzz", fizzBuzz.get(5))
+        assert_equal("buzz", fizzBuzz.get(20))
+    end
+
+    def test_dividable_five_and_three_should_return_fizzbuzz
+        fizzBuzz = FizzBuzz.new
+        assert_equal("fizzbuzz", fizzBuzz.get(15))
+        assert_equal("fizzbuzz", fizzBuzz.get(30))
     end
 end
 
