9901e321de8de5d7a263ff270d99b3ce0fddfead
Croo <croohmanoor@gmail.com>
2013-11-15 19:04:58 +0100

    Finished kata until single custom delimiter

--- StringCalc/stringcalc.rb
+++ StringCalc/stringcalc.rb
@@ -1,6 +1,12 @@
 class StringCalc
 
     def add( string )
-        return string.to_i
+        delimiter = " ";
+        if(string[0] == '\\')
+            delimiter = string.split("\n").first[1..-1]
+        end
+        result = 0
+        string.split(/[#{delimiter}\n]/).each { |number| result += number.to_i }
+        return result
     end
 end
--- StringCalc/stringcalc_test.rb
+++ StringCalc/stringcalc_test.rb
@@ -4,14 +4,35 @@ require_relative "stringcalc"
 
 class StringCalcTest < Test::Unit::TestCase
 
+    def setup
+        @stringCalc = StringCalc.new
+    end
+
     def test_empty_string
-        stringCalc = StringCalc.new
-        assert_equal(0,stringCalc.add(""))
+        assert_equal(0,@stringCalc.add(""))
     end
 
     def test_one_number_returns_same
-        stringCalc = StringCalc.new
-        assert_equal(45,stringCalc.add("45"));
+        assert_equal(45,@stringCalc.add("45"))
     end
 
+    def test_two_number_should_return_sum
+        assert_equal(45,@stringCalc.add("20 25"))
+    end
+
+    def test_any_number_should_return_sum
+        assert_equal(45,@stringCalc.add("10 10 10 3 10 2"))
+    end
+
+    def test_should_handle_newlines
+        assert_equal(45,@stringCalc.add("10 10\n10 3 10 2"))
+    end
+
+    def test_should_support_custom_delimiter
+        assert_equal(45,@stringCalc.add("\\#\n10#10\n10#3#10#2"))
+    end
+
+#    def test_should_support_multiple_custom_delimiter
+#        assert_equal(45,@stringCalc.add("\\#&-_\n10_10\n10&3#10-2"))
+#    end
 end
